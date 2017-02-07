85d6b7dd3714582c8cdd7875f3e07bfb4dcdf479
Brad Thompson <bradleyglind@gmail.com>
2017-02-07 14:08:11 -0700

    IE floating nav

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -2,20 +2,22 @@
   <div class="app-nav">
     <div class="toolbar-placeholder"></div>
 
-    <md-whiteframe id="webapp-toolbar" md-tag="md-toolbar" md-elevation="2">
-      <md-button class="md-icon-button sidebar-toggle" @click="$refs.sidenav.toggle()">
-        <md-icon>menu</md-icon>
-      </md-button>
+    <div id="toolbar-floating-wrapper">
+      <md-whiteframe id="webapp-toolbar" md-tag="md-toolbar" md-elevation="2">
+        <md-button class="md-icon-button sidebar-toggle" @click="$refs.sidenav.toggle()">
+          <md-icon>menu</md-icon>
+        </md-button>
 
-      <h2 class="md-title toolbar-title" style="flex: 1">{{$route.name}}</h2>
+        <h2 class="md-title toolbar-title" style="flex: 1">{{$route.name}}</h2>
 
-      <md-button class="md-icon-button">
-        <md-icon>search</md-icon>
-      </md-button>
-      <md-button class="md-icon-button">
-        <md-icon>more_vert</md-icon>
-      </md-button>
-    </md-whiteframe>
+        <md-button class="md-icon-button">
+          <md-icon>search</md-icon>
+        </md-button>
+        <md-button class="md-icon-button">
+          <md-icon>more_vert</md-icon>
+        </md-button>
+      </md-whiteframe>
+    </div>
 
     <md-sidenav id="webapp-sidebar" md-theme="white" class="md-left md-fixed" ref="sidenav">
       <md-toolbar class="sidebar-logo md-theme-white">
@@ -62,11 +64,20 @@ export default {
 $sidebar-width: 260px;
 $sidebar-stay-open: 1281px;
 
-#webapp-toolbar {
+#toolbar-floating-wrapper {
+  display: flex; // This is to solve the IE off center toolbar buttons
   position: fixed;
   top: 0;
   left: 0;
   right: 0;
+}
+#webapp-toolbar {
+  flex: 1;
+
+  .sidebar-toggle {
+    opacity: 1;
+    transition: opacity 0.4s cubic-bezier(0.25, 0.8, 0.25, 1);
+  }
 }
 
 #webapp-sidebar {
@@ -110,13 +121,11 @@ $sidebar-stay-open: 1281px;
     }
   }
   #webapp-toolbar {
-    left: $sidebar-width;
+    margin-left: $sidebar-width;
 
     .sidebar-toggle {
-      display: none;
-    }
-    .toolbar-title {
-      margin-left: 48px;
+      opacity: 0;
+      pointer-events: none;
     }
   }
 }
